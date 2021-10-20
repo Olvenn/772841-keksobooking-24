6023178f9b9b2e258629f93af2c59910787c69f8
Olvenn <olga-vencel@yandex.ru>
2021-10-20 23:06:03 +0300

    module7-task1-1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import {createOffersArray} from './data.js';
+import {renderCardsList} from './card.js';
+import {makeFormsDisabled, makeFormsActive} from './form.js';
+
+const mapLoad = false;
+
+makeFormsDisabled();
+if (document.readyState === 'DOMContentLoaded' || !mapLoad) {
+  makeFormsActive();
+}
+
+const allAdvertisements = createOffersArray();
+renderCardsList(allAdvertisements);
---
+++ js/util.js
@@ -0,0 +1,82 @@
+const getRandomPositiveInteger =  (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomPositiveFloat = (a, b, digits = 1) => {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+};
+
+const createRandomIdFromRangeGenerator = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomPositiveInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const getNonrepeatingArrayFromArrayRandomLength = (incomeArray) => {
+  const outcomeArray = [];
+  const generatePhotoId = createRandomIdFromRangeGenerator(0, incomeArray.length - 1);
+  for (let i = 0; i <= getRandomPositiveInteger(1, incomeArray.length - 1); i++) {
+    outcomeArray.push(incomeArray[generatePhotoId()]);
+  }
+  return outcomeArray;
+};
+
+const createUniqueIdGenerator  = () => {
+  let lastGeneratedId = 0;
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+const addZeroToBegin = (singleDigits) => {
+  const avatar = String(singleDigits).padStart(2,'0');
+  return avatar;
+};
+
+const checkEnding = (number, words) => {
+  number = Math.abs(number) % 100;
+  const number1 = number % 10;
+  if (number > 10 && number < 20) { return words[2]; }
+  if (number1 > 1 && number1 < 5) { return words[1]; }
+  if (number1 === 1) { return words[0]; }
+  return words[2];
+};
+
+const selectNecessaryElements = ((allElements, necessaryElements, partOfClassName) => {
+  allElements.forEach((element) => {
+    const isNecessary = necessaryElements.some((necessaryElement) => element.classList.contains(`${partOfClassName}${necessaryElement}`));
+    if (!isNecessary) {
+      element.remove();
+    }
+  });
+});
+
+const addClass = (eltment, nameOfClass) => {
+  eltment.classList.add(nameOfClass);
+};
+
+const removeClass = (eltment, nameOfClass) => {
+  eltment.classList.remove(nameOfClass);
+};
+
+export {getRandomPositiveInteger, getRandomPositiveFloat, createRandomIdFromRangeGenerator, getNonrepeatingArrayFromArrayRandomLength, createUniqueIdGenerator, addZeroToBegin};
+
+export {checkEnding, selectNecessaryElements, addClass, removeClass};
